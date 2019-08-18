362bd4c9c9b2b8d61c7130a13cc4e1d326f07948
Marcos Batista <markos.system@gmail.com>
2019-08-18 19:31:27 -0300

    Create run-jenkins.sh

---
+++ run-jenkins.sh
@@ -0,0 +1,11 @@
+docker run -u root --rm \
+-d \
+--memory=640m \
+--memory-swap=1.5g \
+-p 8888:8080 \
+-p 50000:50000 \
+-v jenkins-data:/var/jenkins_home \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-v $(pwd)/apps:/home/apps \
+--name jenkins \
+jenkinsci/blueocean
